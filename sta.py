81edadeb9734173715572a47716a2b82c26223ac
ranchuanjiang <754596974@qq.com>
2018-08-02 22:28:51 +0800

    你好 python  ran'chuan'jiang

--- sta.py
+++ sta.py
@@ -1,1 +1,15 @@
-print("hello world")
+"""
+打印一下图形:
+* * * * *
+*       *
+*       *
+* * * * *
+
+"""
+print("for 循环测试")
+for i in range(4):
+    if i == 0 or i == 3:
+        print("* " * 5)
+    else:
+        print("*\t\t*")
+
