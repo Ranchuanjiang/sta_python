801258caf96f46bc1203650bb4691d8c56466c8e
ranchuanjiang <754596974@qq.com>
2018-08-04 23:16:07 +0800

    开始 str dict 学习

---
+++ sta_str.py
@@ -0,0 +1,19 @@
+# 字符串得简单操作
+# 连接操作
+s1 = "i love"
+s2 = "hahahaha "
+s3 = s1 + s2
+print(s3)
+print(s1 + " " + s2)
+
+#  字符串得乘法
+
+print(s1 * 5)
+
+# 字符串当成列表
+print(s1[1])
+print(s1[-1])
+print(s1[2:4])
+print(s1[:])
+
+
