5145a619ab8ce94a77ba9dc9791c1286a7a8f3de
ranchuanjiang <754596974@qq.com>
2018-08-05 09:32:06 +0800

    开始 str dict 学习

--- sta_str.py
+++ sta_str.py
@@ -15,20 +15,26 @@ print(s1[1])
 print(s1[-1])
 print(s1[2:4])
 print(s1[:])
-# 首字母大写 其余小写
+#  capitalize首字母大写 其余小写
 s3 = "i love You "
 print(s3.capitalize())
-# 每个单词首字母大写
+
+# title每个单词首字母大写
 s4 = "i love you"
 print(s4.title())
-# 字母全部大写
+# upper字母全部大写
 s5 = "i love you"
 print(s5.upper())
-# 字母全部小写
+# lower字母全部小写
 s6 = "I LOVE YOU"
 print(s6.lower())
-# 字母小写变大写 大写变小写
+# swapcase字母小写变大写 大写变小写
 s7 = "I love YOU"
 print(s7.swapcase())
-
+# len 字符串长度空格也占位
+print(len(s7))
+# find() 查找指定字符串,找不到返回-1 第一次找到返回第一次索引值
+# index() 查找制定字符串, 找不到报错
+print(s7.find("w"))
+print(s7.index("w"))
 
